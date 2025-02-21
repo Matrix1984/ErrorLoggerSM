834890ae6839dcd0c66b3b4aa711de2f191a51d4
Matrix1984 <pentester1984@outlook.com>
2025-02-21 09:33:15 +0200

    [Addition] GetSeverityWithPaginationQuery.

---
+++ src/Application/SeverityLevels/Query/GetSeverityWithPaginationQuery.cs
@@ -0,0 +1,32 @@
+﻿using ErrorLoggerSM.Application.Common.Interfaces;
+using ErrorLoggerSM.Application.Common.Mappings;
+using ErrorLoggerSM.Application.Common.Models;
+
+namespace ErrorLoggerSM.Application.SeverityLevels.Query;
+
+public record GetSeverityWithPaginationQuery : IRequest<PaginatedList<SeverityLevelDto>>
+{
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
+
+public class GetSeverityWithPaginationQueryHandler : IRequestHandler<GetSeverityWithPaginationQuery, PaginatedList<SeverityLevelDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetSeverityWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<SeverityLevelDto>> Handle(GetSeverityWithPaginationQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.SeverityLevels
+               .OrderBy(x => x.Id)
+               .ProjectTo<SeverityLevelDto>(_mapper.ConfigurationProvider)
+               .PaginatedListAsync(request.PageNumber, request.PageSize);
+
+    }
+}
