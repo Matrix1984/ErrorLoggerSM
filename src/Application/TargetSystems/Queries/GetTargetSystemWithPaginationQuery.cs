0a8dc27d8f180a8de8ddb33df9e0d75055cef178
Matrix1984 <pentester1984@outlook.com>
2025-02-21 16:08:37 +0200

    [Addition] TargetSystems endpoint.

--- src/Application/TargetSystems/Queries/GetTargetSystemWithPaginationQuery.cs
+++ src/Application/TargetSystems/Queries/GetTargetSystemWithPaginationQuery.cs
@@ -5,14 +5,13 @@ using ErrorLoggerSM.Application.TargetApps.Query;
 
 namespace ErrorLoggerSM.Application.TargetSystems.Queries;
 
-public record GetTargetSystemWithPaginationQuery : IRequest<PaginatedList<TargetAppDto>>
-{
-    public int ListId { get; init; }
+public record GetTargetSystemWithPaginationQuery : IRequest<PaginatedList<TargetSystemDto>>
+{ 
     public int PageNumber { get; init; } = 1;
     public int PageSize { get; init; } = 10;
 }
 
-public class GetTargetAppWithPaginationQueryHandler : IRequestHandler<GetTargetSystemWithPaginationQuery, PaginatedList<TargetAppDto>>
+public class GetTargetAppWithPaginationQueryHandler : IRequestHandler<GetTargetSystemWithPaginationQuery, PaginatedList<TargetSystemDto>>
 {
     private readonly IApplicationDbContext _context;
     private readonly IMapper _mapper;
@@ -23,11 +22,11 @@ public class GetTargetAppWithPaginationQueryHandler : IRequestHandler<GetTargetS
         _mapper = mapper;
     }
 
-    public async Task<PaginatedList<TargetAppDto>> Handle(GetTargetSystemWithPaginationQuery request, CancellationToken cancellationToken)
+    public async Task<PaginatedList<TargetSystemDto>> Handle(GetTargetSystemWithPaginationQuery request, CancellationToken cancellationToken)
     {
         return await _context.TargetApps
             .OrderBy(x => x.Id)
-            .ProjectTo<TargetAppDto>(_mapper.ConfigurationProvider)
+            .ProjectTo<TargetSystemDto>(_mapper.ConfigurationProvider)
             .PaginatedListAsync(request.PageNumber, request.PageSize);
     }
 }
