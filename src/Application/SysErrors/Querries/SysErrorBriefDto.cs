3eb7d572725f728645a5f0e5ea9dd4952634c19a
Matrix1984 <pentester1984@outlook.com>
2025-02-21 15:57:17 +0200

    [Addition] SysError endpoint.

--- src/Application/SysErrors/Querries/SysErrorBriefDto.cs
+++ src/Application/SysErrors/Querries/SysErrorBriefDto.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ErrorLoggerSM.Domain.Entities;
-using ErrorLoggerSM.Domain.Enums;
-
-namespace ErrorLoggerSM.Application.SysErrors.Querries;
+﻿namespace ErrorLoggerSM.Application.SysErrors.Querries;
 public class SysErrorBriefDto
-{ 
+{
     public int Id { get; set; }
     public int? TargetHttpErrorCode { get; set; }
-    public string? TargetAppErrorCode { get; set; } 
-    public string? TargetShortDescription { get; set; }   
-    public string? ErrorLogTypeName { get; set; } 
+    public string? TargetAppErrorCode { get; set; }
+    public string? TargetShortDescription { get; set; }
+    public string? ErrorLogTypeName { get; set; }
     public DateTimeOffset? ErrorGeneratedDateTime { get; set; }
-    public string TargetAppName { get; set; } = null!; 
+    public string TargetAppName { get; set; } = null!;
     public string TargetSystemName { get; set; } = null!;
 }
---
+++ src/Web/Endpoints/SysErrors.cs
@@ -0,0 +1,35 @@
+﻿using ErrorLoggerSM.Application.Common.Models;
+using ErrorLoggerSM.Application.SysErrors.Commands.CreateSysError;
+using ErrorLoggerSM.Application.SysErrors.Querries;
+using ErrorLoggerSM.Domain.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ErrorLoggerSM.Web.Endpoints;
+ 
+public class SysErrors : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .RequireAuthorization()
+            .MapGet(GetSysErrorsWithPagination)
+            .MapPost(CreateSysError);
+    }
+
+    public async Task<Ok<PaginatedList<SysErrorBriefDto>>> GetSysErrorsWithPagination(ISender sender, [AsParameters] GetSysErrorsWithPaginationQuery query)
+    {
+        var result = await sender.Send(query);
+
+        return TypedResults.Ok(result);
+    }
+
+    public async Task<Created<int>> CreateSysError(ISender sender, CreateSysErrorCommand command)
+    {
+        var id = await sender.Send(command);
+
+        return TypedResults.Created($"/{nameof(SysError)}/{id}", id);
+    }
+
+
+
+}
