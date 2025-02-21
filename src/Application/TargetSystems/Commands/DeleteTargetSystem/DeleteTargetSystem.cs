e1b6c90b09df990440a2f718f06ac6ef7a043dd7
Matrix1984 <pentester1984@outlook.com>
2025-02-21 10:06:00 +0200

    [Addition] DeleteTargetSystemCommand.

---
+++ src/Application/TargetSystems/Commands/DeleteTargetSystem/DeleteTargetSystem.cs
@@ -0,0 +1,27 @@
+﻿using ErrorLoggerSM.Application.Common.Interfaces;
+
+namespace ErrorLoggerSM.Application.TargetSystems.Commands.DeleteTargetSystem;
+public record DeleteTargetSystemCommand(int Id) : IRequest;
+
+public class DeleteTodoListCommandHandler : IRequestHandler<DeleteTargetSystemCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteTodoListCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteTargetSystemCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.TargetSystems
+            .Where(l => l.Id == request.Id)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        Guard.Against.NotFound(request.Id, entity);
+
+        _context.TargetSystems.Remove(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
