cc4408f11fadb90dbd0fe6e347419e0d91d0e45f
Matrix1984 <pentester1984@outlook.com>
2025-02-21 10:02:36 +0200

    [Addition] DeleteErrorEntityCommand.

---
+++ src/Application/ErrorEntities/Commands/DeleteErrorEntity/DeleteErrorEntity.cs
@@ -0,0 +1,28 @@
+﻿using ErrorLoggerSM.Application.Common.Interfaces;
+
+namespace ErrorLoggerSM.Application.ErrorEntities.Commands.DeleteErrorEntity;
+
+public record DeleteErrorEntityCommand(int Id) : IRequest;
+
+public class DeleteErrorEntityCommandHandler : IRequestHandler<DeleteErrorEntityCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteErrorEntityCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteErrorEntityCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.ErrorEntities
+            .Where(l => l.Id == request.Id)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        Guard.Against.NotFound(request.Id, entity);
+
+        _context.ErrorEntities.Remove(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
