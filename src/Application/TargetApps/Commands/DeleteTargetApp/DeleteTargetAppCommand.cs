95c7ad892f6aca9ed6d5c185c5aa29314885efe8
Matrix1984 <pentester1984@outlook.com>
2025-02-21 09:36:55 +0200

    [Addition] DeleteTargetAppCommand.

---
+++ src/Application/TargetApps/Commands/DeleteTargetApp/DeleteTargetAppCommand.cs
@@ -0,0 +1,28 @@
+﻿using ErrorLoggerSM.Application.Common.Interfaces;
+
+namespace ErrorLoggerSM.Application.TargetApps.Commands.DeleteTargetApp;
+
+public record DeleteTargetAppCommand(int Id) : IRequest;
+
+public class DeleteTodoListCommandHandler : IRequestHandler<DeleteTargetAppCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteTodoListCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteTargetAppCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.TargetApps
+            .Where(l => l.Id == request.Id)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        Guard.Against.NotFound(request.Id, entity);
+
+        _context.TargetApps.Remove(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
